e4b3c796387b5f5331270c12c88d068a3ae6b143
Shaishav-Kr <84002444+Shaishav-Kr@users.noreply.github.com>
2023-06-17 21:59:08 +0530

    new changes

--- lemon_restaurant/src/App.test.js
+++ lemon_restaurant/src/App.test.js
@@ -1,12 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-import { Reservations } from './components';
-
-test('Checking Time Status Section', () => {
-  render( <Reservations />);
-
-  const statusHeadaing = screen.getByText("Evening");
-
-  expect(statusHeadaing).toBeInTheDocument();
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
 });
